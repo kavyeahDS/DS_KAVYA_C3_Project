dd50545f9c970f12a77c4d160337ff8d6ef66050
kavyeahDS <109661477+kavyeahDS@users.noreply.github.com>
2022-07-20 18:16:19 +0530

    Update Restaurant.java

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -18,14 +18,14 @@ public class Restaurant {
     }
 
     public boolean isRestaurantOpen() {
-        return true;
-        //DELETE ABOVE STATEMENT AND WRITE CODE HERE
+          LocalTime currtime = getCurrentTime();
+        return openingTime.isBefore(currtime) && currtime.isBefore(closingTime);
     }
 
     public LocalTime getCurrentTime(){ return  LocalTime.now(); }
 
     public List<Item> getMenu() {
-        return null;
+        return menu;
         //DELETE ABOVE RETURN STATEMENT AND WRITE CODE HERE
     }
 
